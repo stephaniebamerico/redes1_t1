31a1d6e977662602099a79a336f067ad5cdd3397
Talita <thcf16@inf.ufpr.br>
2017-11-27 17:07:30 -0200

    aaaaaaaaaaa
    Signed-off-by: Talita <thcf16@inf.ufpr.br>

--- src/servidor.cpp
+++ src/servidor.cpp
@@ -101,7 +101,7 @@ int main(int argc, char const *argv[]) {
             }
             else if (msg_recebida->tipo == PUT) 
             {
-                pede_arquivo(socket, "stdout", PUT);
+                pede_arquivo(socket, msg_recebida->dados, PUT);
             }
         }
     }
