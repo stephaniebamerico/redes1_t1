853f8d055f64adce3a62c5bab793d5c4f3db034b
Stephanie Briere <sba16@inf.ufpr.br>
2017-11-27 21:53:43 -0200

    nao volta janela

--- src/raw_socket.cpp
+++ src/raw_socket.cpp
@@ -129,6 +129,7 @@ int recebe_conteudo(int socket, mensagem_t ***msg) {
                 ultimo_envio = time(NULL);
             }
             else {
+                printf("sequencia fora do esperado\n");
                 // timeout para ACK: reseta janela
                 /*for (i = 0; i <= 3 && inicio-i > 0 && (inicio-i)%TAM_SEQUENCIA != seq; ++i);
                 if((inicio-i)%TAM_SEQUENCIA == seq) {
